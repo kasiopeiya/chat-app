616f7a137fecd9a87e99e2afdc97c5fd364cd489
kasiopeiya <67563058+kasiopeiya@users.noreply.github.com>
2020-08-05 19:16:40 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAKDy8V2E8y2rAsQ4UfByQ2Q7nXkNdahBs',
+  authDomain: 'chatapp-fe0b0.firebaseapp.com',
+  databaseURL: 'https://chatapp-fe0b0.firebaseio.com',
+  projectId: 'chatapp-fe0b0',
+  storageBucket: 'chatapp-fe0b0.appspot.com',
+  messagingSenderId: '927199564108',
+  appId: '1:927199564108:web:b111e414b66dc8188d3fe9',
+  measurementId: 'G-K3NQPSTTV8',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
